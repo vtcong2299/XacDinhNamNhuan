78455bb9d2ccc832d509bd60d286366815477667
vtcong2299 <vtcong2299@gmail.com>
2024-08-07 21:49:14 +0700

    Commit 1

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+ 
+
+namespace XacDinhNamNhuan
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            int year;
+            Console.Write("Nhap vao nam muon xac dinh: ");
+            year = int .Parse(Console.ReadLine());
+            
+            
+            if ((year%4==0 && year%100!=0) || year%400==0)
+            {
+                Console.WriteLine("Nam {0} la nam nhuan: ", year);
+            }
+            
+            else 
+            Console.WriteLine("Nam {0} khong phai la nam nhuan: ", year);
+            
+            
+            Console.ReadKey();
+        }
+    }
+}
